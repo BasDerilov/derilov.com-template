7c6a48d8523f036f131338aaa106c9d706f9b62b
Vasil Derilov <vasil.derilov@gmail.com>
2021-12-07 17:02:43 +0300

    Finished main page and added elements to Contact and Games

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,22 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+const regexp = /(rgb\(\d+, ?\d+, ?\d+\)), (rgb\(\d+, ?\d+, ?\d+\))/g;
+
+const body = document.querySelector("body");
+const bodyBgData = getComputedStyle(body).getPropertyValue("background");
+const colors = [...bodyBgData.matchAll(regexp), 'red', 'green'];
+
+const firstColor = colors[0][1];
+const secondColor = colors[0][2];
+
+const navLinks = document.querySelectorAll("a");
+const banners = document.getElementsByClassName("banner")
+
+navLinks.forEach(element => {
+    element.style.cssText = 'color: ' + secondColor +';';
+});
+
+// for(let i = 0; i < banners.length; i++){
+//     banners[i].style.cssText += 'background: linear-gradient(45deg,' + firstColor + ', ' + secondColor + ');';
+// }
